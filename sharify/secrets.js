cb39bec64f7473369f45c82aeda147f00187610c
Harris McCullers <harrismccullers@gmail.com>
2019-10-23 17:12:20 -0700

    removed unused files and removed node_modules from repo

---
+++ sharify/secrets.js
@@ -0,0 +1,8 @@
+//this files stores the spotify dev information
+//make sure this in in the .gitignore
+
+export const spotifyCredentials = {
+    clientId: 'Your Client ID',
+    clientSecret: 'Your Client Secret',
+    redirectUri: 'Your Redirect URI'
+    }
