bc0908211476f114ac22a41bf5ff11c39f9d67da
Harris McCullers <harrismccullers@gmail.com>
2019-10-29 14:04:27 -0700

    Redirect URI changes

--- sharify/secrets.js
+++ sharify/secrets.js
@@ -4,5 +4,5 @@
 export const spotifyCredentials = {
     clientId: '2b9a23d90f084beeb0ad9a433ae5588f',
     clientSecret: '56b063cc9bc14ffe999815b3e9f874e2',
-    redirectUri: 'https://auth.expo.io/@harrismcc/Authenticator'
+    redirectUri: 'https://auth.expo.io/@121team7/Authenticator'
     }
